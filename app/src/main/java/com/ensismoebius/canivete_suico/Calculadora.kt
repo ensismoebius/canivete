675824cbc334ec700700324a2cbba3fd8c14598d
Andre Furlan <ensismoebius@gmail.com>
2022-06-09 09:02:19 -0300

    Calculadora pronta

--- app/src/main/java/com/ensismoebius/canivete_suico/Calculadora.kt
+++ app/src/main/java/com/ensismoebius/canivete_suico/Calculadora.kt
@@ -1,11 +1,109 @@
 package com.ensismoebius.canivete_suico
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Calculadora : AppCompatActivity() {
+
+    private val avaliador: EvaluateString = EvaluateString()
+    private val txtformula: TextView by lazy { findViewById(R.id.txtFormula) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calculadora)
+
+        val zero: Button = findViewById(R.id.btn0)
+        zero.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "0"
+        }
+
+        val um: Button = findViewById(R.id.btn1)
+        um.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "1"
+        }
+
+        val dois: Button = findViewById(R.id.btn2)
+        dois.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "2"
+        }
+
+        val tres: Button = findViewById(R.id.btn3)
+        tres.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "3"
+        }
+
+        val quatro: Button = findViewById(R.id.btn4)
+        quatro.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "4"
+        }
+
+        val cinco: Button = findViewById(R.id.btn5)
+        cinco.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "5"
+        }
+
+        val seis: Button = findViewById(R.id.btn6)
+        seis.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "6"
+        }
+
+        val sete: Button = findViewById(R.id.btn7)
+        sete.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "7"
+        }
+
+        val oito: Button = findViewById(R.id.btn8)
+        oito.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "8"
+        }
+
+        val nove: Button = findViewById(R.id.btn9)
+        nove.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "9"
+        }
+
+        val limpa: Button = findViewById(R.id.btnCe)
+        limpa.setOnClickListener {
+            txtformula.text = ""
+        }
+
+        val mais: Button = findViewById(R.id.btnSoma)
+        mais.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "+"
+        }
+
+        val menos: Button = findViewById(R.id.btnSubtracao)
+        menos.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "-"
+        }
+
+
+        val mult: Button = findViewById(R.id.btnMultiplicacao)
+        mult.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "*"
+        }
+
+        val div: Button = findViewById(R.id.btnDivisao)
+        div.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "/"
+        }
+
+        val abre: Button = findViewById(R.id.btnAbreParenteses)
+        abre.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + "("
+        }
+
+        val fecha: Button = findViewById(R.id.btnFecharParenteses)
+        fecha.setOnClickListener {
+            txtformula.text = txtformula.text.toString() + ")"
+        }
+
+        val igual: Button = findViewById(R.id.btnIgual)
+        igual.setOnClickListener {
+            val expr: String = txtformula.text.toString()
+            txtformula.text = avaliador.evaluate(expr)
+        }
     }
 }
---
+++ app/src/main/java/com/ensismoebius/canivete_suico/EvaluateString.kt
@@ -0,0 +1,184 @@
+package com.ensismoebius.canivete_suico
+
+import java.util.*
+
+class EvaluateString {
+
+    /**
+     * Evaluate given expression and returns the result
+     *
+     * @param expression
+     * @return
+     */
+    fun evaluate(expression: String): String? {
+
+        // Ignores empty expressions
+        if (expression.trim().isEmpty()) {
+            return ""
+        }
+
+        // An array of tokens
+        val tokens = expression.toCharArray()
+
+        // Stack for numbers: 'values'
+        val values: Stack<String> = Stack<String>()
+
+        // Stack for Operators: 'ops'
+        val ops: Stack<Char> = Stack<Char>()
+
+        // Cicles over all tokens in expression
+        var i = -1
+
+        while (++i < tokens.size) {
+
+            // Current token is a whitespace, skip it
+            if (tokens[i] == ' ') {
+                continue
+            }
+
+            // Current token is a number, push it to stack for numbers
+            if (tokens[i] in '0'..'9' || Character.isAlphabetic(tokens[i].code)) {
+
+                // There may be more than one digits in number
+                val sbuf = StringBuffer()
+                while (i < tokens.size && (isPartOfANumber(tokens[i]) || isPartOfAVariable(tokens[i]))) {
+                    sbuf.append(tokens[i++])
+                }
+
+                // After number has been built push it into the numbers stack
+                values.push(sbuf.toString())
+
+                // We must subtract 1 at the index because the next
+                // interaction will add 1, doing this we can keep track
+                // of the tokens
+                i--
+
+                // After build the number goto to next char/iteration
+                continue
+            }
+
+            // Current token is an opening brace, push it to 'ops'
+            if (tokens[i] == '(') {
+                ops.push(tokens[i])
+                i++
+                continue
+            }
+
+            // Closing brace encountered, solve entire brace
+            if (tokens[i] == ')') {
+                while (ops.peek() !== '(') {
+                    values.push(applyOperation(ops.pop(), values.pop(), values.pop()))
+                }
+                ops.pop()
+                i++
+                continue
+            }
+
+            // Current token is an operator.
+            if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/' || tokens[i] == '=') {
+                // While top of 'ops' has same or greater precedence to current
+                // token, which is an operator. Apply operator on top of 'ops'
+                // to top two elements in values stack
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek())) {
+                    values.push(applyOperation(ops.pop(), values.pop(), values.pop()))
+                }
+
+                // Push current token to 'ops'.
+                ops.push(tokens[i])
+            }
+        }
+
+        // Entire expression has been parsed at this point, apply remaining
+        // ops to remaining values
+        while (!ops.empty()) {
+            values.push(applyOperation(ops.pop(), values.pop(), values.pop()))
+        }
+
+        // Top of 'values' contains result, return it
+        return values.pop()
+    }
+
+    private fun isPartOfAVariable(c: Char): Boolean {
+        return Character.isAlphabetic(c.code)
+    }
+
+    /**
+     * Verify if a char may be a part of a number
+     *
+     * @param c
+     * @return true if is a part of a number, false if its not
+     */
+    private fun isPartOfANumber(c: Char): Boolean {
+        if (c in '0'..'9') {
+            return true
+        }
+        return c == '.'
+    }
+
+    /**
+     * Returns true if 'op2' has higher or same precedence as 'op1', otherwise
+     * returns false.
+     *
+     * @param op1
+     * @param op2
+     * @return
+     */
+    private fun hasPrecedence(op1: Char, op2: Char): Boolean {
+        if (op2 == '=') return false
+        if (op2 == '(' || op2 == ')') return false
+        return !((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+    }
+
+    private fun isNumber(value: String?): Boolean {
+        try {
+            value!!.toDouble()
+            return true
+        } catch (e: Exception) {
+        }
+        return false
+    }
+
+    /**
+     * A utility method to apply an operator 'op' on operands 'a' and 'b'. Return
+     * the result
+     *
+     * @param operation
+     * @param secondNumber
+     * @param firstNumber
+     * @return
+     */
+    private fun applyOperation(operation: Char, valueTwo: String, valueOne: String): String? {
+        var valueTwo: String? = valueTwo
+        val isValueOneNumeric = isNumber(valueOne)
+        val isValueTwoNumeric = isNumber(valueTwo)
+        var firstNumber = 0.0
+        var secondNumber = 0.0
+        if (operation != '=') {
+            if (isValueOneNumeric) {
+                firstNumber = valueOne.toDouble()
+            }
+        }
+        if (isValueTwoNumeric) {
+            if (valueTwo != null) {
+                secondNumber = valueTwo.toDouble()
+            }
+        }
+
+        var result = 0.0
+        when (operation) {
+            '+' -> result = firstNumber + secondNumber
+            '-' -> result = firstNumber - secondNumber
+            '*' -> result = firstNumber * secondNumber
+            '=' -> {
+                result = 0.0
+            }
+            '/' -> {
+                if (secondNumber == 0.0) {
+                    throw UnsupportedOperationException("Cannot divide by zero")
+                }
+                result = firstNumber / secondNumber
+            }
+        }
+        return result.toString()
+    }
+}
