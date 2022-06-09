37aba164d91471e3b533af69e49c51697b0a3fd3
Andre Furlan <ensismoebius@gmail.com>
2022-06-09 00:35:20 -0300

    Theme

--- app/src/main/java/com/ensismoebius/canivete_suico/MainActivity.kt
+++ app/src/main/java/com/ensismoebius/canivete_suico/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.ensismoebius.canivete_suico
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCalculadora: Button = findViewById(R.id.btnCalculadora)
+        btnCalculadora.setOnClickListener {
+            abrirCalculadora()
+        }
     }
 
     private fun abrirCalculadora() {
